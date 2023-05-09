18e9ab25f5bb78d307957902420238c4d57a62b8
Arthur Svensson <guimaraesarthur86@yahoo.com.br>
2023-05-08 22:57:38 -0300

    Aula 13 e 14: Cucumber e Runner

---
+++ src/test/java/com/agsvensson/runner/RunnerTest.java
@@ -0,0 +1,18 @@
+package com.agsvensson.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+        features = "src/test/resources/features/",
+        tags = "",
+        glue = "src/test/java/com/agsvensson/steps/",
+        plugin = {"json:target/reports/cucumberReports.json", "pretty"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+)
+
+public class RunnerTest {
+
+}
