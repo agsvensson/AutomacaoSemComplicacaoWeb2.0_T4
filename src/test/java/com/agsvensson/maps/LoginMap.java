2fad6cdbc313d1dd10aae64ba3ec9cfe3445db59
Arthur Svensson <guimaraesarthur86@yahoo.com.br>
2023-05-09 23:07:17 -0300

    Aula 16 e 17: Pages, tags, wait, try catch

--- src/test/java/com/agsvensson/maps/LoginMap.java
+++ src/test/java/com/agsvensson/maps/LoginMap.java
@@ -1,15 +1,23 @@
 package com.agsvensson.maps;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginMap {
+    @FindBy(css = "#menuUser")
     public WebElement btnLogin;
+    @FindBy(css = ".closeBtn")
     public WebElement btnFechar;
+    @FindBy(css = ".PopUp")
     public WebElement divFecharModal;
+    @FindBy(css = "input[name='username']")
     public WebElement inpUserName;
+    @FindBy(css = "input[name='password']")
     public WebElement inpPassword;
+    @FindBy(xpath = "(//button[normalize-space()='SIGN IN'])")
     public WebElement btnSignIn;
+    @FindBy(xpath = "//input[@name='remember_me']")
     public WebElement inpRemember;
+    @FindBy(linkText = "CREATE NEW ACCOUNT")
     public WebElement linkCreateAccount;
-
 }
